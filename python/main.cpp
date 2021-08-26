5d9ad23252199133f565406cdfef7e3a3d749fe2
Tom de Geus <tom@geus.me>
2021-08-26 10:23:24 +0200

    Fixing submodule

--- python/main.cpp
+++ python/main.cpp
@@ -12,10 +12,6 @@
 #include "Cartesian2d.hpp"
 #include "Cartesian3d.hpp"
 
-// Enable basic assertions on matrix shape
-// (doesn't cost a lot of time, but avoids segmentation faults)
-#define GMATTENSOR_ENABLE_ASSERT
-
 namespace py = pybind11;
 
 PYBIND11_MODULE(_GMatTensor, m)
